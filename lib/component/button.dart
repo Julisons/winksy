cbc75c8c118b2a36573ff513235dbf9f644a46dd
Austines <austines@spa-limited.com>
2025-06-28 11:35:33 +0300

    MJM

--- lib/component/button.dart
+++ lib/component/button.dart
@@ -47,7 +47,7 @@ class IButton extends StatelessWidget {
                 onPress();
               },
         child: Container(
-           height:height,
+           height:height == 45 ? 40.h : height,
             decoration: BoxDecoration(
                 color: /*isDisabled
                     ? xDisabledColor
